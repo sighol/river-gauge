ac724762f462a7dd0d4f8f9ae8d8e4308a2218d7
Sigurd Holsen <sighol@gmail.com>
2019-04-10 21:46:03 +0200

    Experiment with functions and reading data

--- gauge/sim.ino
+++ gauge/sim.ino
@@ -1,6 +1,7 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(11, 3); // RX, TX
+SoftwareSerial simSerial(11, 3); // RX, TX
+
 
 void sim_setup() {
   // Open serial communications and wait for port to open:
@@ -13,14 +14,38 @@ void sim_setup() {
   Serial.println("Hardware serial working");
 
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
+  simSerial.begin(9600);
+  while (!simSerial) {
+    ;
+  }
+
+  Serial.println("SIM800 serial working");
+
+  String response = askSimCard("at+gmr");
+  Serial.print("Message sent: ");
+  Serial.println(response);
 }
 
-void sim_loop() { // run over and over
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
+void sim_loop() {
+  while (simSerial.available()) {
+    byte simSerialResult = simSerial.read();
+    Serial.write(simSerialResult);
   }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
+}
+
+String askSimCard(String question) {
+  simSerial.print(question);
+  simSerial.print("\r");
+
+  char response[100];
+  int i;
+  for (i = 0; simSerial.available(); i++)
+  {
+    response[i] = simSerial.read();
+    Serial.println(response[i]);
   }
+
+  response[i] = '\0';
+
+  return String(response);
 }

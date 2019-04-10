0094d6c017549601aaf73ccced77d22c2d9acfe3
Sigurd Holsen <sighol@gmail.com>
2019-04-10 19:50:26 +0200

    Add Karsten kode for parsing ultrasound as distance

--- gauge.ino
+++ gauge.ino
@@ -1,9 +1,10 @@
 void setup() {
+    ultrasonic_setup();
   // put your setup code here, to run once:
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    ultrasonic_loop();
 }

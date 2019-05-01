38df91e2d47003c73f0f98020f0290c37db1a926
Sigurd Holsen <sighol@gmail.com>
2019-05-01 16:01:59 +0200

    experiment with libraries

--- gauge/gauge.ino
+++ gauge/gauge.ino
@@ -1,9 +1,50 @@
+#include <sim800.h>
+#include <SoftwareSerial.h>
+#include <ultrasound.h>
+
+SoftwareSerial simSerial(11, 3); // RX, TX
+
+void connectHardwareSerial()
+{
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
+    Serial.println("Hardware serial working");
+}
+
+void connectSoftwareSerial()
+{
+    simSerial.begin(9600);
+    while (!simSerial) {
+        ;
+    }
+    Serial.println("SIM800 serial working");
+}
+
+Sim800 sim(&simSerial);
+Ultra ultra (9, 10);
 
 void setup() {
-  sim_setup();
+    connectHardwareSerial();
+    connectSoftwareSerial();
+
+    ultra.start();
 }
 
 void loop() {
+    delay(1000);
+    double distance = ultra.readDistance();
+    Serial.print("Distance is: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    char url[100];
+    strcat(url, "http://hvorerdetvann.com/post_saggrenda_data/");
+    char distance_str[10];
+    dtostrf(distance, 6, 2, distance_str);
+    strcat(url, distance_str);
+    sim.sendHttpGet(url);
   // put your main code here, to run repeatedly:
-  sim_loop();
 }
